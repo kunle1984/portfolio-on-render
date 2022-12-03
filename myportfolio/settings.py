42f33a8ff2ae8b7d41949792742cc4c1e5c40b9b
kunle <kunlefes089@gmail.com>
2022-12-03 16:01:16 +0100

    change database

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -80,12 +80,24 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
+    'default': {
+         #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'wdVYhj65bTvcfrWhgYbZ',
+        'HOST': 'containers-us-west-74.railway.app',
+        'PORT': '7073',
+    }
+}
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
 
 
